e1aa2adcb989b20293d6c8f77791d04987d590ef
DesBasito <basit.03.Kg@gmail.com>
2023-12-27 16:49:06 +0600

    added start methods

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -9,9 +9,50 @@ public class TaskManager {
         this.tasks = new ArrayList<>();
     }
 
-    // Методы для добавления, изменения, удаления, отображения и сортировки задач
-    // ...
 
-    // Методы для считывания/выгрузки задач из/в JSON файла
-    // ...
+    // конструктор, геттеры и сеттеры
+
+    public void displayMenu() {
+        // Вывести меню взаимодействия с пользователем
+    }
+
+    public void showAllTasks() {
+        // Вывести все задачи
+    }
+
+    public void addNewTask() {
+        // Добавить новую задачу
+    }
+
+    public void changeTask() {
+        // Изменить статус или описание задачи
+    }
+
+    public void deleteTask() {
+        // Удалить задачу
+    }
+
+    public void saveToJson() {
+        // Сохранить задачи в JSON файл
+    }
+
+    public void loadFromJson() {
+        // Загрузить задачи из JSON файла
+    }
+
+    public void markOverdueTasks() {
+        // Пометить просроченные задачи
+    }
+
+    public void sortTasksByPriority() {
+        // Отсортировать задачи по приоритету
+    }
+
+    public void sortTasksByCreationDate() {
+        // Отсортировать задачи по дате создания
+    }
+
+    public void sortTasksByDescription() {
+        // Отсортировать задачи по описанию
+    }
 }
