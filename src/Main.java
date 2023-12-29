a5154c60cec989a716b1a75784dcedcf72e8f7b6
Danis Kurenkiev <kurenkievdd@gmail.com>
2023-12-29 18:07:09 +0600

    Finished state pattern

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,5 @@ public class Main {
     private static final List<Task> tasks = FileUtil.readFile();
 
     public static void main(String[] args) {
-        for (Task task : tasks) {
-            task.displayTask();
-        }
     }
 }
